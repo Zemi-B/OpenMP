58b60a556c0ee6ec612a7438014e09247e8ce7d7
kuhaku-space <te4958yu6924@gmail.com>
2022-01-06 14:47:00 +0900

    Update debug.sh

--- src/debug.sh
+++ src/debug.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm out.txt
+mkdir ../build
 for program_number in "1" "2" "3"; do
   for num_threads in 1 2 4 8; do
     gcc main$program_number.c -Wall -Wextra -fopenmp -O3 -DNUM_THREADS=$num_threads -DDEBUG -o ../build/main$program_number.out
@@ -9,4 +10,5 @@ for program_number in "1" "2" "3"; do
   done
 done
 
+mkdir ../img
 python3 fig.py
