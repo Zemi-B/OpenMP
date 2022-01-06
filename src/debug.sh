f44a25807959556390cd127d27d086b886b4548f
kuhaku-space <te4958yu6924@gmail.com>
2022-01-06 15:44:41 +0900

    Update debug.sh

--- src/debug.sh
+++ src/debug.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 rm out.txt
-mkdir ../build
+mkdir -p ../build
 for program_number in "1" "2" "3"; do
   for num_threads in 1 2 4 8; do
     gcc main$program_number.c -Wall -Wextra -fopenmp -O3 -DNUM_THREADS=$num_threads -DDEBUG -o ../build/main$program_number.out
-    echo --- main$program_number \($num_threads thread\) ---
     ../build/main$program_number.out ../data/fhoto.pgm ../data/fhoto$program_number.pgm >>out.txt
   done
 done
 
-mkdir ../img
+mkdir -p ../img
 python3 fig.py
