fb4629ed424f6da16809dfcb62e2fc3a47a45f85
kuhaku-space <te4958yu6924@gmail.com>
2022-01-07 10:28:54 +0900

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ../build
 for program_number in "1" "2" "3"; do
-  for num_threads in 1 2 4 8; do
+  for num_threads in 1 2 4 8 16 24; do
     gcc main$program_number.c -Wall -Wextra -fopenmp -O3 -DNUM_THREADS=$num_threads -o ../build/main$program_number.out
     echo --- main$program_number \($num_threads thread\) ---
     ../build/main$program_number.out ../data/fhoto.pgm ../data/fhoto$program_number.pgm
