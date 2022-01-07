11d4c2b08b5435bc1b5c68d7deccb0465ede0c53
kuhaku-space <te4958yu6924@gmail.com>
2022-01-07 10:28:47 +0900

    Update debug.sh

--- src/debug.sh
+++ src/debug.sh
@@ -3,7 +3,7 @@
 rm out.txt
 mkdir -p ../build
 for program_number in "1" "2" "3"; do
-  for num_threads in 1 2 4 8; do
+  for num_threads in 1 2 4 8 16 24; do
     gcc main$program_number.c -Wall -Wextra -fopenmp -O3 -DNUM_THREADS=$num_threads -DDEBUG -o ../build/main$program_number.out
     ../build/main$program_number.out ../data/fhoto.pgm ../data/fhoto$program_number.pgm >>out.txt
   done
