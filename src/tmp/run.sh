612017ea848df3845a7a7b3d7b565057cbaebc43
kuhaku-space <te4958yu6924@gmail.com>
2022-01-07 11:07:12 +0900

    Update run.sh

--- src/tmp/run.sh
+++ src/tmp/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-gcc main.c -Wall -Wextra -fopenmp -O3 -o main.out
-./main.out
+mkdir -p ../build
+  for num_threads in 1 2 4 8 16 24; do
+    gcc main.c -Wall -Wextra -fopenmp -O3 -DNUM_THREADS=$num_threads -o ../build/main.out
+    echo --- main \($num_threads thread\) ---
+    ../build/main.out ../../data/fhoto.pgm ../../data/fhoto_tmp.pgm
+  done
+done
